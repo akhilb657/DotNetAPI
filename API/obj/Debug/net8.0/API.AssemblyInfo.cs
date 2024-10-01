b0e8e28d416c97c29e3c2e998694f92ca9928fcb
akhil <akhilboyanapalli657@gmail.com>
2024-10-01 15:45:11 -0500

    move endpoint out of Program.cs

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+07eea08f23e6525c2a37ba82331939289066b688")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2a5236d939414ee2a8adbfec4fedc3a9b2d4167c")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
