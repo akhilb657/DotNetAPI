50d76116b65f7ea1a219300b8c049d444f2ee9b9
akhil <akhilboyanapalli657@gmail.com>
2024-10-02 18:35:10 -0500

    endpoints for GetUsers and GetSingleUser

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6d252acc5eb32d1b8dc3aec532007285d0a719ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a0af6051dde232598c0f5d451840de7ee05d66b")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
