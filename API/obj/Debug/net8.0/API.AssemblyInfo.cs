49301cc556d4366fa65bcd01795c14899a457143
akhil <akhilboyanapalli657@gmail.com>
2024-10-04 17:53:19 -0500

    EF UserController

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f6116960e6813c5acc17f3522e757c62661d92a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e565df6981d63a6931fb012c5b025a92a97e109")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
