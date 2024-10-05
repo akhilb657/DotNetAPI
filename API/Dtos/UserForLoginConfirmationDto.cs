72011d6c3a72389ff456c181922fe5a4d49df027
akhil <akhilboyanapalli657@gmail.com>
2024-10-05 15:49:38 -0500

    Auth settings and Dtos

---
+++ API/Dtos/UserForLoginConfirmationDto.cs
@@ -0,0 +1,21 @@
+namespace API.Dtos;
+{
+  partial class UserForLoginConfirmationDto
+  {
+    byte[] PasswordHash {get; set;}
+    byte[] PasswordSalt {get; set;}
+
+    UserForLoginConfirmationDto()
+    {
+        if(PasswordHash == null)
+        {
+          PasswordHash = new byte[0];
+        }
+
+          if(PasswordSalt == null)
+        {
+          PasswordSalt = new byte[0];
+        }
+    }
+  }
+}
