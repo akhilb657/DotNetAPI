2480dff627d8bb17e0085e9bfbb09e1c6917bfa8
akhil <akhilboyanapalli657@gmail.com>
2024-10-14 14:07:40 -0500

    Insert and Update - UserSalary, UserJobInfo Tables

--- API/Stored Procedures/UserUpsert.sql
+++ API/Stored Procedures/UserUpsert.sql
@@ -6,6 +6,9 @@ CREATE OR ALTER PROCEDURE TutorialAppSchema.spUser_Upsert
 	@LastName NVARCHAR(50),
 	@Email NVARCHAR(50),
 	@Gender NVARCHAR(50),
+    @JobTitle NVARCHAR(50),
+    @Department NVARCHAR(50),
+    @Salary DECIMAL(18,4),
 	@Active BIT = 1,
     @UserId INT = NULL
 AS
@@ -14,6 +17,7 @@ BEGIN
         BEGIN
             IF NOT EXISTS (SELECT * FROM TutorialAppSchema.Users WHERE Email = @Email)
             BEGIN
+                DECLARE @OutputUserId INT
 
                 INSERT INTO TutorialAppSchema.Users(
                     [FirstName],
@@ -27,6 +31,25 @@ BEGIN
                         @Gender,
                         @Active
                     )
+                SET @OutputUserId = @@IDENTITY
+
+                INSERT INTO TutorialAppSchema.UserSalary(
+                    UserId,
+                    Salary
+                ) VALUES (
+                    @OutputUserId,
+                    @Salary
+                )
+
+                INSERT INTO TutorialAppSchema.UserJobInfo(
+                    UserId,
+                    Department,
+                    JobTitle
+                ) VALUES (
+                    @OutputUserId,
+                    @Department,
+                    @JobTitle
+                )
             END
         END
     ELSE 
@@ -37,6 +60,15 @@ BEGIN
                     Email = @Email,
                     Gender = @Gender,
                     Active = @Active
+                WHERE UserId = @UserId
+            
+            UPDATE TutorialAppSchema.UserSalary
+                SET Salary = @Salary
+                WHERE UserId = @UserId
+            
+            UPDATE TutorialAppSchema.UserJobInfo
+                SET Department = @Department,
+                    JobTitle = @JobTitle
                 WHERE UserId = @UserId
         END
 END
