d2fefd35e3ad5f0ea90d876d9e8a76a09c04f741
akhil <akhilboyanapalli657@gmail.com>
2024-10-20 11:46:28 -0500

    replace Add and Update endpoints with SP Upsert post

--- API/obj/Debug/net8.0/API.AssemblyInfo.cs
+++ API/obj/Debug/net8.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f1a7cd7f9a82371a1a0b5e5f4400a9a0081ae053")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6e35f2e4129ba1be4bae1b44edf1103abb09eeaf")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
